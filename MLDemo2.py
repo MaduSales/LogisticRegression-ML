a0b49b61eeaedaedb665aa3cec0b623a26692e7d
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-07-28 21:55:38 -0300

    Update MLDemo2.py

--- MLDemo2.py
+++ MLDemo2.py
@@ -9,3 +9,15 @@ iris_data.head()
 #Split data into features (X) and labels (y)
 X = iris_data.drop(columns=['Id', 'Species'])
 y = iris_data['Species']
+
+#Create a ML Model
+model = LogisticRegression()
+
+#Training the model
+model.fit(X.values, y)
+
+#Predict using the trained model
+predictions = model.predict([[4.6, 3.5, 1.5, 0.2]])
+
+#Print the predictions
+print(predictions)
